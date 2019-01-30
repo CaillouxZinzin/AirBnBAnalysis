26c9073155e80b240f76fe522f81bcbd24d3cf66
CaillouxZinzin <pierre.romon@etu.utc.fr>
2019-01-30 16:13:34 +0100

    Route for lyon and bordeaux

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -91,6 +91,12 @@ def graphsBerlin():
     berlin_reviews_number = [3, 107, 617, 3092, 7422, 17702, 39470, 69303, 111180, 152450]
     berlin_reviews_plot = build_graph(reviews_date, berlin_reviews_number)
 
+    #top10
+
+    lot_of_rev = berlin_listings.nlargest(100, "number_of_reviews")
+    berlin_top_10 = lot_of_rev.nlargest(10, "review_scores_rating")
+    berlin_top_10 = berlin_top_10.loc[:,['id', 'room_type', 'host_id', 'host_since', 'reviews_per_month', 'review_scores_rating', 'price', 'zipcode', 'number_of_reviews']]
+
     return render_template('berlin.html',
         mean_price_berlin = round(mean_price_berlin, 2),
         hist_berlin=hist_berlin,
@@ -102,6 +108,102 @@ def graphsBerlin():
         berlin_private_room_mean_price= round(berlin_private_room.price.mean(), 2),
         berlin_entire_house_mean_price= round(berlin_entire_house.price.mean(), 2),
         berlin_reviews_plot= berlin_reviews_plot,
+        debug= 'debug'
+    )
+
+@app.route('/lyon')
+def graphsLyon():
+
+    lyon_listings = pd.read_csv(LYON_PATH + "clean_lyon_listing.csv", low_memory=False)
+    lyon_reviews = pd.read_csv(LYON_PATH + "year_reviews.csv", low_memory=False)
+
+    lyon_listings["price"] = pd.to_numeric(lyon_listings["price"])
+    mean_price_lyon = lyon_listings.price.mean()
+
+    price_lyon = lyon_listings['price']
+    hist_lyon = build_hist_paris(price_lyon)
+
+    total_houses_lyon = len(lyon_listings)
+
+    #type of room
+
+    lyon_room = lyon_listings
+    lyon_entire_house = lyon_room.loc[lyon_room['room_type']=="Entire home/apt"]
+    lyon_private_room = lyon_room.loc[lyon_room['room_type']=="Private room"]
+    lyon_total_private_room = len(lyon_private_room)
+    lyon_total_entire_house = len(lyon_entire_house)
+    lyon_percentage_entire_house = (lyon_total_entire_house/total_houses_lyon)*100
+    lyon_percentage_private_room = (lyon_total_private_room/total_houses_lyon)*100
+
+    reviews_date = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+    lyon_reviews_number = [0, 4, 69, 397, 1470, 4968, 13775, 32628, 51774, 66418]
+    lyon_reviews_plot = build_graph(reviews_date, lyon_reviews_number)
+
+    #top10
+
+    lot_of_rev = lyon_listings.nlargest(100, "number_of_reviews")
+    lyon_top_10 = lot_of_rev.nlargest(10, "review_scores_rating")
+    lyon_top_10 = lyon_top_10.loc[:,['id', 'room_type', 'host_id', 'host_since', 'reviews_per_month', 'review_scores_rating', 'price', 'zipcode', 'number_of_reviews']]
+
+    return render_template('lyon.html',
+        mean_price_lyon = round(mean_price_lyon, 2),
+        hist_lyon=hist_lyon,
+        total_houses_lyon= total_houses_lyon,
+        lyon_total_private_room= lyon_total_private_room,
+        lyon_total_entire_house= lyon_total_entire_house,
+        lyon_percentage_entire_house= round(lyon_percentage_entire_house,3),
+        lyon_percentage_private_room= round(lyon_percentage_private_room,3),
+        lyon_private_room_mean_price= round(lyon_private_room.price.mean(), 2),
+        lyon_entire_house_mean_price= round(lyon_entire_house.price.mean(), 2),
+        lyon_reviews_plot= lyon_reviews_plot,
+        debug= 'debug'
+    )
+
+@app.route('/bordeaux')
+def graphsBordeaux():
+
+    bdx_listings = pd.read_csv(BDX_PATH + "clean_bdx_listing.csv", low_memory=False)
+    bdx_reviews = pd.read_csv(BDX_PATH + "year_reviews.csv", low_memory=False)
+
+    bdx_listings["price"] = pd.to_numeric(bdx_listings["price"])
+    mean_price_bdx = bdx_listings.price.mean()
+
+    price_bdx = bdx_listings['price']
+    hist_bdx = build_hist_paris(price_bdx)
+
+    total_houses_bdx = len(bdx_listings)
+
+    #type of room
+
+    bdx_room = bdx_listings
+    bdx_entire_house = bdx_room.loc[bdx_room['room_type']=="Entire home/apt"]
+    bdx_private_room = bdx_room.loc[bdx_room['room_type']=="Private room"]
+    bdx_total_private_room = len(bdx_private_room)
+    bdx_total_entire_house = len(bdx_entire_house)
+    bdx_percentage_entire_house = (bdx_total_entire_house/total_houses_bdx)*100
+    bdx_percentage_private_room = (bdx_total_private_room/total_houses_bdx)*100
+
+    reviews_date = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+    bdx_reviews_number = [0, 1, 60, 178, 1060, 3890, 13112, 32394, 55376, 75189]
+    bdx_reviews_plot = build_graph(reviews_date, bdx_reviews_number)
+
+    #top10
+
+    lot_of_rev = bdx_listings.nlargest(100, "number_of_reviews")
+    bdx_top_10 = lot_of_rev.nlargest(10, "review_scores_rating")
+    bdx_top_10 = bdx_top_10.loc[:,['id', 'room_type', 'host_id', 'host_since', 'reviews_per_month', 'review_scores_rating', 'price', 'zipcode', 'number_of_reviews']]
+
+    return render_template('bordeaux.html',
+        mean_price_bdx = round(mean_price_bdx, 2),
+        hist_bdx=hist_bdx,
+        total_houses_bdx= total_houses_bdx,
+        bdx_total_private_room= bdx_total_private_room,
+        bdx_total_entire_house= bdx_total_entire_house,
+        bdx_percentage_entire_house= round(bdx_percentage_entire_house,3),
+        bdx_percentage_private_room= round(bdx_percentage_private_room,3),
+        bdx_private_room_mean_price= round(bdx_private_room.price.mean(), 2),
+        bdx_entire_house_mean_price= round(bdx_entire_house.price.mean(), 2),
+        bdx_reviews_plot= bdx_reviews_plot,
         debug= 'debug'
     )
 
